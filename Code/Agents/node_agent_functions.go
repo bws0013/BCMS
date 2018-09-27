f7293d0ab34671e7a0b1c90494638e6e84d36799
Ben <i@auburn.edu>
2018-09-26 18:31:24 -0600

    adding some more functions

--- Code/Agents/node_agent_functions.go
+++ Code/Agents/node_agent_functions.go
@@ -2,6 +2,7 @@ package main
 
 import (
   // "fmt"
+  "errors"
   "io/ioutil"
   "log"
   "strings"
@@ -22,8 +23,11 @@ func create_sym_link(source_file_path, destination_file_path string) error {
 }
 
 // Check if a file exists at the location
-func contains_existing_file(destination_file_path string) bool {
-  return false
+func contains_file(destination_file_path string) bool {
+  if _, err := os.Stat(destination_file_path); !os.IsNotExist(err) {
+    return false
+  }
+  return true
 }
 
 // This will write to a file, check existence/permission prior
@@ -36,7 +40,6 @@ func add_file(file []byte, destination_file_path string) error {
     The global_ids of the files currently on the system
 */
 func get_static_table_of_contents(filepath string) {
-
   var files []string
   files = make([]string, 0)
   file_contents, err := read_file(filepath)
@@ -45,20 +48,12 @@ func get_static_table_of_contents(filepath string) {
   for _, line := range file_lines {
     files = append(files, line)
   }
-
   // return files
 }
 
 func get_in_memory_table_of_contents() error {
   return nil
 }
-
-// func read_file(filename string) []string {
-// 	fileBytes, err := ioutil.ReadFile(filename)
-// 	c(err)
-// 	//fmt.Println("HTML:\n\n", string(fileBytes))
-// 	return string(fileBytes)
-// }
 
 func read_file(filename string) (string, error) {
   fileBytes, err := ioutil.ReadFile(filename)
